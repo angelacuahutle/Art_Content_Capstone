ff6fbf5880f5c2f17c133fa56663bdb05b000105
angelacuahutle <angela.cuahutle@gmail.com>
2021-09-26 23:53:45 -0500

    add unit test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_most(20) }
+  end
+
+  describe 'assocations' do
+
+  end
 end

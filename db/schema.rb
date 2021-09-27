5400cb648c55f906822c3deee2a52dfa5bb9f28e
angelacuahutle <angela.cuahutle@gmail.com>
2021-09-27 15:56:19 -0500

    add author_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_27_191706) do
+ActiveRecord::Schema.define(version: 2021_09_27_192616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,9 +40,12 @@ ActiveRecord::Schema.define(version: 2021_09_27_191706) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_users_on_author_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "articles", "categories"
+  add_foreign_key "users", "users", column: "author_id"
 end

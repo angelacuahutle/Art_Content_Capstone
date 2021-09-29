c60576844db5d24b60855533516c19334d234d5d
angelacuahutle <angela.cuahutle@gmail.com>
2021-09-29 17:42:37 -0500

    refactor articles controllers

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,43 +7,35 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
 
-    if @article.save
-      format.html { redirect_to @article, notice: 'Article was successfully created.' }
-      format.json { render :show, status: :created, location: @article }
-    else
-      format.html { render :new, status: :unprocessable_entity }
-      format.json { render json: @article.errors, status: :unprocessable_entity }
+    respond_to do |format|
+      if @article.save
+        format.html { redirect_to @article, notice: 'Article was successfully created.' }
+        format.json { render :show, status: :created, location: @article }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
     end
   end
-end
 
-def show; end
-
-def edit
-  unless @current_user == @article.author
-    redirect_to article_path, notice: 'Seems like this article does not belong to you!'
+  def update
+    respond_to do |format|
+      if @article.update(article_params)
+        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
+        format.json { render :show, status: :ok, location: @article }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
-  if @article.update(article_params)
-    redirect_to @article
-  else
-    render 'edit'
-  end
-end
-
-def update
-  if @article.update(article_params)
-    redirect_to @article
-  else
-    render 'edit'
-  end
-end
-
-def destroy
-  if @article.destroy do |format|
-    format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
-    format.json { head :no_content }
-  end
+  def destroy
+    @article.destroy
+    respond_to do |format|
+      format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private

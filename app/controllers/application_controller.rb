3c31157f1b122e3a40667040c2e9dd1536e720e8
angelacuahutle <angela.cuahutle@gmail.com>
2021-10-01 16:30:06 -0500

    fix linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
-  def index
-  end
+  def index; end
 
   protected
 
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   describe 'validations' do
-    it {should validate_presence_of(:priority)}
-    it {should have_many(:articles)}
+    it { should validate_presence_of(:priority) }
+    it { should have_many(:articles) }
   end
 end

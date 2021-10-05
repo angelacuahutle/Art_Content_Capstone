70a4bcbf0266bc4d1254dbade6faebff1e32ff71
angelacuahutle <angela.cuahutle@gmail.com>
2021-10-05 17:52:07 -0500

    properly implement website layout with bootstrap, fix navbar and add categories, relocate javascript resources

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+  before_action :create_categories_buttons
 
   def index; end
 
@@ -8,5 +9,22 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def set_current_user
+    if session[:id]
+      @current_user ||= User.find(session[:id])
+    else
+      @current_user = nil
+    end
+  end
+
+  def authenticate_user
+    flash[:alert] = 'Please login!'
+    redirect_to sessions_path if session[:id].nil?
+  end
+
+  def create_categories_buttons
+    @top_categories = Category.order(:priority).limit(5)
   end
 end

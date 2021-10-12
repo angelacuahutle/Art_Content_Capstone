e4d0d704ac9dbe4a45d9e3f999a8ee47204b56c1
angelacuahutle <angela.cuahutle@gmail.com>
2021-10-11 21:23:38 -0500

    refactor article info in helper

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,6 +1,5 @@
 module VotesHelper
   def vote_button(article)
-    # binding.pry
     vote = Vote.find_by(user: current_user, article: article)
     if vote
       link_to('Unvote', article_vote_path(id: vote.id, article_id: article.id),
@@ -8,5 +7,10 @@ module VotesHelper
     else
       link_to('Vote', article_vote_path(article), method: :post)
     end
+  end
+
+  def info_of(article)
+    content_tag :h3, @article.author.name.html_safe
+    "Total votes: #{@article.votes.all.count}".html_safe
   end
 end

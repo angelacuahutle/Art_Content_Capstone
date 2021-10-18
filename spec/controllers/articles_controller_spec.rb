dbe8bcaf93072cf1d235bda0f6de47cdf36ced46
angelacuahutle <angela.cuahutle@gmail.com>
2021-10-18 00:27:38 -0500

    add devise config, set initial article controller test

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe ArticlesController, type: :controller do
   describe 'GET #index' do
-    # expect to 
+    it do
+      get :index
+      is_expected.to redirect_to new_user_session_path
+    end
   end
 end

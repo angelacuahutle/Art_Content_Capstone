ed4412480884497f493f3e2a371cc388bee2f2a3
angelacuahutle <angela.cuahutle@gmail.com>
2021-10-18 12:38:32 -0500

    add articles controller

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ArticlesController, type: :controller do
+  let(:valid_session) { {} }
+
   describe 'GET #index' do
-    it do
-      get :index
-      is_expected.to redirect_to new_user_session_path
+    it 'redirects to login' do
+      get :index, params: {}, session: valid_session
+      expect(response).to redirect_to new_user_session_path
     end
   end
 end

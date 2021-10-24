e25c247f50bb17cfb2b1e878700991d9c25890a4
angelacuahutle <angela.cuahutle@gmail.com>
2021-10-24 16:58:05 -0500

    add scope sort_by most recent

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,10 +6,13 @@ class Article < ApplicationRecord
   validates :body, presence: true
   has_one_attached :image
 
+  scope :most_recent_top4, -> { order(created_at: :desc).limit(4) }
+
   def self.top_article
     return unless Vote.any?
 
     article_id = Vote.group(:article_id).count.max_by { |_, v| v }.first
     Article.find(article_id)
   end
+
 end
